4f129340cfd17b5b715fb56650f2c7a295f27b94
mathis.villemin <mathis.villemin@epita.fr>
2022-01-19 02:27:34 +0100

    liaison entre menu == done

---
+++ Unity_project/Assets/Scripts/Menu_script/change_menu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class change_menu : MonoBehaviour
+{
+    public void PlayCampain()
+    {
+        SceneManager.LoadScene("Campain");
+    }
+    public void setting_Menu()
+    {
+    }
+}
