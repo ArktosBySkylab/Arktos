906ebcac21c5388d4e73077f7f50fd9cf7763874
Gregoire Lefaure <gregoire.lefaure@epita.fr>
2022-03-05 20:44:06 +0100

    prefabs reorganized

--- Unity_project/Assets/Scripts/Levels/GameMaster.cs
+++ Unity_project/Assets/Scripts/Levels/GameMaster.cs
@@ -7,16 +7,20 @@ namespace Levels
 
     public class GameMaster : MonoBehaviour
     {
-        [SerializeField] protected List<GameObject> players;
+        //[SerializeField] protected List<GameObject> players;
         protected (int, int) startCoords = (0, 0);
 
         public void Start()
         {
             Debug.Log("COUCOU C'EST MOI TCHOUPI");
-            foreach (GameObject player in players)
-            {
-                Instantiate(player, new Vector3(startCoords.Item1, startCoords.Item2), Quaternion.identity).SetActive(true);
-            }
+            //foreach (GameObject player in players)
+            //{
+            //    Instantiate(player, new Vector3(startCoords.Item1, startCoords.Item2), Quaternion.identity).SetActive(true);
+            //}
+            
+            // TEMPORARY PART (pour la premiere soutenance uniquement)
+            GameObject character = Resources.Load<GameObject>("Kitsune");
+            Instantiate(character, new Vector3(startCoords.Item1, startCoords.Item2), Quaternion.identity).SetActive(true);
         }
     }
 }
