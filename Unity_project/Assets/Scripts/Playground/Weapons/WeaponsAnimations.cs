44bb9d77eb38549b4ed1b7b3ba0df2ca8f8d35b0
Gregoire Lefaure <gregoire.lefaure@epita.fr>
2022-04-15 15:19:03 +0200

    weapons disappear after try shooting

--- Unity_project/Assets/Scripts/Playground/Weapons/WeaponsAnimations.cs
+++ Unity_project/Assets/Scripts/Playground/Weapons/WeaponsAnimations.cs
@@ -4,24 +4,18 @@ namespace Playground.Weapons
 {
     public class WeaponsAnimations : StateMachineBehaviour
     {
-        public Weapon weapon;
-        
         public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo,
             int layerIndex)
         {
+            Debug.Log("LOULO" + animator.gameObject);
+            animator.SetInteger("IsFighting", 0);
         }
 
         public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo,
             int layerIndex)
         {
-            animator.SetInteger("IsFighting", 0);
-            Debug.Log("OnStateExit");
-            Debug.Log(animator.name);
-            if (!(weapon is null))
-            {
-                Debug.Log(weapon.gameObject.GetComponent<SpriteRenderer>());
-                weapon.gameObject.GetComponent<SpriteRenderer>().enabled = false;
-            }
+            if (animator.gameObject.CompareTag("Weapons"))
+                animator.gameObject.GetComponent<SpriteRenderer>().enabled = false;
         }
 
         public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo,
